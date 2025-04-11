31a863e1e7809212bce36048f1c3b7560256af56
Alvise Favero <66018479+alvi-se@users.noreply.github.com>
2025-04-11 22:53:19 +0200

    feat: betting

--- contracts/HorseRace.sol
+++ contracts/HorseRace.sol
@@ -10,28 +10,44 @@ contract HorseRace {
 
     struct Race {
         uint[] horses;
-        uint horseCount;
-        mapping (address => uint) bets;
+        mapping (uint => mapping (address => uint)) bets;
         bool finished;
+        address creator;
     }
+
     
     constructor() {
         owner = msg.sender;
     }
 
-    function bet() public payable {
+    function bet(uint raceId, uint horseId) public payable {
+        require(msg.value > 0, "Negative amount was sent");
+        require(raceId < races.length, "No such race exists");
+        Race storage race = races[raceId];
 
+        require(!race.finished, "Can't bet on finished race");
+        require(horseId < race.horses.length, "Horse index out of range");
+        
+        race.bets[horseId][msg.sender] += msg.value;
     }
 
-    function prepareRace(uint horseCount) public {
-        require(races[races.length - 1].finished, "The last race has not finished");
-        /*
+    function createRace(uint horseCount) public returns (uint) {
 
-        Race race = Race({
-            horses: new uint[horseCount]
-        });
-        */
+        Race storage race = races.push();
+        race.horses = new uint[](horseCount);
+        race.creator = msg.sender;
+        race.finished = false;
 
+        return races.length - 1;
     }
 
+    function startRace(uint raceIndex) public {
+        require(raceIndex < races.length, "Race does not exist");
+
+        Race storage race = races[raceIndex];
+        require(msg.sender == race.creator, "You are not the creator of this race");
+        require(!race.finished, "Race already finished");
+
+        // Finish
+    }
 }
