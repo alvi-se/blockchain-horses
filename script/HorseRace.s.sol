28545e114c99ad9cf401a6676c553e446ddf1a2c
Alvise Favero <66018479+alvi-se@users.noreply.github.com>
2025-04-22 00:41:33 +0200

    docs: fix to vrf mocking on local environment

--- script/HorseRace.s.sol
+++ script/HorseRace.s.sol
@@ -7,6 +7,11 @@ import '../src/HorseRace.sol';
 import '../src/Shuffler.sol';
 import '@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol';
 
+/*
+To fix overflow/underflow errors in the script, check here
+https://github.com/smartcontractkit/chainlink-brownie-contracts/issues/33
+ */
+
 
 contract HorseRaceScript is Script {
     VRFCoordinatorV2_5Mock vrfMock;
@@ -17,7 +22,7 @@ contract HorseRaceScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        vrfMock = new VRFCoordinatorV2_5Mock(100000000000000000, 1000000000, 1000);
+        vrfMock = new VRFCoordinatorV2_5Mock(100_000_000_000_000_000, 1000000000, 1000);
         vrfSubscription = vrfMock.createSubscription();
         vrfMock.fundSubscription(vrfSubscription, 100000000000000000000); // 100 LINK
 
@@ -26,5 +31,10 @@ contract HorseRaceScript is Script {
         shuffler.setHorseRace(address(horseRace));
 
         vm.stopBroadcast();
+
+        console.log('VRF Subscription ID:', vrfSubscription);
+        console.log('VRF Coordinator Address:', address(vrfMock));
+        console.log('Shuffler Address:', address(shuffler));
+        console.log('HorseRace Address:', address(horseRace));
     }
 }
