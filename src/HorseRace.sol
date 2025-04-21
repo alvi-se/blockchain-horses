bb9f4d7c0e5fb3a162d751481f2e33c94e00489e
Alvise Favero <66018479+alvi-se@users.noreply.github.com>
2025-04-21 21:41:42 +0200

    feat: shuffler, race handling

--- src/HorseRace.sol
+++ src/HorseRace.sol
@@ -2,52 +2,151 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
+import './Shuffler.sol';
 
 contract HorseRace {
 
-    address public owner;
+    // ---------- Variables ----------
+
+    address private owner;
     Race[] public races;
+    Shuffler private shuffler;
+    uint constant private HOUSE_EDGE = 69;
+
+    // Tells whether the address has a bet on raceId
+    // This is needed to quickly check if the address has a bet on a race
+    mapping (address => mapping (uint256 => bool)) private betOwners;
+    // Same as above but for race owners
+    mapping (address => mapping (uint256 => bool)) private raceOwners;
+
+    mapping (address => uint256) private winnings;
+
+    // ---------- Structs --------
 
     struct Race {
-        uint[] horses;
-        mapping (uint => mapping (address => uint)) bets;
-        bool finished;
+        uint256[] horses;
+        // Map horseId to address, and address to amount
+        // mapping (uint256 => mapping (address => uint256)) bets;
+        // Bet[] bets;
+        mapping (uint256 => Bet[]) bets;
+        uint256 winningHorse;
+        RaceStatus status;
         address creator;
     }
 
+    enum RaceStatus {
+        NOT_STARTED,
+        IN_PROGRESS,
+        FINISHED
+    }
+
+    struct Bet {
+        address owner;
+        uint256 horseId;
+        uint256 amount;
+    }
     
+
+    // ---------- Modifiers ----------
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    modifier onlyShuffler() {
+        require(msg.sender == address(shuffler), "You are not the shuffler");
+        _;
+    }
+
+
+    // ---------- Constructor ----------
+
     constructor() {
         owner = msg.sender;
     }
 
-    function bet(uint raceId, uint horseId) public payable {
+    // ---------- Functions ----------
+
+    function bet(uint256 raceId, uint256 horseId) external payable {
         require(msg.value > 0, "Negative amount was sent");
         require(raceId < races.length, "No such race exists");
         Race storage race = races[raceId];
 
-        require(!race.finished, "Can't bet on finished race");
+        require(race.status == RaceStatus.NOT_STARTED, "Can't bet on started race");
         require(horseId < race.horses.length, "Horse index out of range");
+
+        race.bets[horseId].push(Bet({
+            owner: msg.sender,
+            horseId: horseId,
+            amount: msg.value
+        }));
         
-        race.bets[horseId][msg.sender] += msg.value;
+        // race.bets[horseId][msg.sender] += msg.value;
+        if (!betOwners[msg.sender][raceId]) {
+            betOwners[msg.sender][raceId] = true;
+        }
     }
 
-    function createRace(uint horseCount) public returns (uint) {
+    function createRace(uint256 horseCount) external returns (uint256) {
+        require(horseCount < 256, "Horse count is too high");
 
         Race storage race = races.push();
-        race.horses = new uint[](horseCount);
+        race.horses = new uint256[](horseCount);
         race.creator = msg.sender;
-        race.finished = false;
+        race.status = RaceStatus.NOT_STARTED;
+
+        raceOwners[msg.sender][races.length - 1] = true;
 
         return races.length - 1;
     }
 
-    function startRace(uint raceIndex) public {
+    function startRace(uint256 raceIndex) external {
         require(raceIndex < races.length, "Race does not exist");
 
         Race storage race = races[raceIndex];
         require(msg.sender == race.creator, "You are not the creator of this race");
-        require(!race.finished, "Race already finished");
+        require(race.status == RaceStatus.NOT_STARTED, "Race already finished");
 
-        // Finish
+        race.status = RaceStatus.IN_PROGRESS;
+
+        uint32 horseCount = uint32(race.horses.length);
+
+        shuffler.shuffle(raceIndex, horseCount);
+    }
+
+    function onRaceFinish(uint256 raceId, uint256[] calldata randomWords) external onlyShuffler {
+        Race storage race = races[raceId];
+        race.status = RaceStatus.FINISHED;
+        race.horses = randomWords;
+
+        uint256 min = type(uint256).max;
+
+        for (uint256 i = 0; i < randomWords.length; i++) {
+            if (randomWords[i] < min) {
+                min = i;
+            }
+        }
+        race.winningHorse = min;
+
+        Bet[] storage winningBets = race.bets[min];
+
+        for (uint256 i = 0; i < winningBets.length; i++) {
+            Bet storage bet = winningBets[i];
+            uint256 amount = bet.amount;
+            // TODO check if the computation is correct, Copilot wrote it
+            uint256 winningsAmount = (amount * (1000 - HOUSE_EDGE)) / 1000;
+            winnings[bet.owner] += winningsAmount;
+        }
+    }
+
+
+    function withdraw() external {
+        uint256 amount = winnings[msg.sender];
+        require(amount > 0, "No winnings to withdraw");
+        // FIRST put the amount to 0, then transfer, to avoid re-entrancy attacks
+        winnings[msg.sender] = 0;
+        address payable to = payable(msg.sender);
+        to.transfer(amount);
     }
 }
---
+++ src/Shuffler.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {HorseRace} from "./HorseRace.sol";
+
+contract Shuffler is VRFConsumerBaseV2Plus {
+
+    // ---------- Variables ----------
+
+    uint256 s_subscriptionId;
+    address vrfCoordinator = 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B;
+    bytes32 s_keyHash =
+        0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae;
+    uint32 callbackGasLimit = 40000;
+    uint16 requestConfirmations = 3;
+    // uint32 numWords = 1;
+
+    // Map requestId to raceId
+    mapping(uint256 => uint256) private requestsToRaces;
+    HorseRace private horseRace;
+
+    // ---------- Structs ----------
+
+    struct Request {
+        uint256 requestId;
+        uint256 raceId;
+    }
+
+    constructor(uint256 subscriptionId) VRFConsumerBaseV2Plus(vrfCoordinator) {
+        s_subscriptionId = subscriptionId;
+    }
+
+
+    // ---------- Modifiers ----------
+
+    modifier onlyHorseRace() {
+        require(msg.sender == address(horseRace), "You are not the horse race contract");
+        _;
+    }
+
+
+    // ---------- Events ----------
+
+    event StartShuffling(uint256 requestId, uint256 indexed raceId);
+    event Shuffled(uint256 requestId, uint256 indexed raceId);
+
+    // ---------- Functions ----------
+
+
+    function setHorseRace(address horseRaceAddress) external onlyOwner {
+        horseRace = HorseRace(horseRaceAddress);
+    }
+
+
+    function shuffle(
+        uint256 raceId,
+        uint32 horseCount
+    ) external onlyHorseRace returns (uint256) {
+        uint requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: horseCount,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Pay with native token
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: true})
+                )
+            })
+        );
+
+        emit StartShuffling(requestId, raceId);
+        return requestId;
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        uint256 raceId = requestsToRaces[requestId];
+        emit Shuffled(requestId, raceId);
+        horseRace.onRaceFinish(raceId, randomWords);
+    }
+}
